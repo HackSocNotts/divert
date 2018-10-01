b2101c993d39cadef38082024d42fa0ce7ae7a6a
Aaron Osher <aaron@oshers.com>
2018-10-02 00:46:19 +0100

    Update redir_links.js

--- redir_links.js
+++ redir_links.js
@@ -1,4 +1,3 @@
 window.nexdivert = {
-	"twitter": "https://twitter.com/@_nexerq",
-	"onedrivereg": "https://drive.google.com/file/d/1e9VOJoVPmvoRa5zn86DH4afHF9T_1qhT/view?usp=sharing"
+	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
 };
