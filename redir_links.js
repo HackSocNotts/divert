886cc9d64b57c88fe92626db6a5427cb76f0370a
Aaron Osher <aaron@aaronosher.io>
2018-10-02 00:50:30 +0100

    Update redir_links.js
    Add constitutional amendment links

--- redir_links.js
+++ redir_links.js
@@ -1,3 +1,4 @@
 window.nexdivert = {
 	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
+	"ammend": "https://docs.google.com/forms/d/e/1FAIpQLSccHWJctuY1Mp-5j5kdLf2cXWCx2dG8hweneJUwKqY06v3lOA/viewform"
 };
