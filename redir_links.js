cbea96268c373b8a822bd7faa8b1614b936180ec
Aaron Osher <aaron@oshers.com>
2018-10-02 20:29:19 +0100

    Update redir_links.js
    Add CS Access Form

--- redir_links.js
+++ redir_links.js
@@ -1,5 +1,6 @@
 window.nexdivert = {
 	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
 	"ammend": "https://docs.google.com/forms/d/e/1FAIpQLSccHWJctuY1Mp-5j5kdLf2cXWCx2dG8hweneJUwKqY06v3lOA/viewform",
-	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view"
+	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view",
+	"cs-access": "https://docs.google.com/forms/d/e/1FAIpQLSeL2pzmlY7m3L9E_SKCiwymwibTPeZicUZzelCqkXhXOMcB2A/viewform",
 };
