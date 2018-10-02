c0f276470655c034c29f72ffbc2afc5b904297b9
Aaron Osher <aaron@oshers.com>
2018-10-02 01:36:24 +0100

    Update redir_links.js
    Add October 1st ICS Link

--- redir_links.js
+++ redir_links.js
@@ -1,4 +1,5 @@
 window.nexdivert = {
 	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
-	"ammend": "https://docs.google.com/forms/d/e/1FAIpQLSccHWJctuY1Mp-5j5kdLf2cXWCx2dG8hweneJUwKqY06v3lOA/viewform"
+	"ammend": "https://docs.google.com/forms/d/e/1FAIpQLSccHWJctuY1Mp-5j5kdLf2cXWCx2dG8hweneJUwKqY06v3lOA/viewform",
+	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view"
 };
