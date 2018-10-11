c6e2fd293ac7d1f03e06f931512bcf2b3c6be4f8
Aaron Osher <aaron@oshers.com>
2018-10-11 13:52:02 +0100

    Update redir_links.js

--- redir_links.js
+++ redir_links.js
@@ -4,5 +4,6 @@ window.nexdivert = {
 	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view",
 	"cs-access": "https://docs.google.com/forms/d/e/1FAIpQLSeL2pzmlY7m3L9E_SKCiwymwibTPeZicUZzelCqkXhXOMcB2A/viewform",
 	"hardware-poll": "https://hacksoc.typeform.com/to/a9Flsq",
-	"python-2": "https://github.com/monotron/python-workshops/blob/master/worksheet-session2.md"
+	"python-2": "https://github.com/monotron/python-workshops/blob/master/worksheet-session2.md",
+	"hacktoberfest-slides": "https://slides.com/aaronosher/hacksoc-hacktoberfest/live#/",
 };
