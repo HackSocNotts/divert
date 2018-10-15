ae43604974b648c965cb4bdb9d704733cc8a978c
Aaron Osher <aaron@oshers.com>
2018-10-15 21:04:12 +0100

    Update redir_links.js

--- redir_links.js
+++ redir_links.js
@@ -1,6 +1,5 @@
 window.nexdivert = {
 	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
-	"ammend": "https://docs.google.com/forms/d/e/1FAIpQLSccHWJctuY1Mp-5j5kdLf2cXWCx2dG8hweneJUwKqY06v3lOA/viewform",
 	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view",
 	"cs-access": "https://docs.google.com/forms/d/e/1FAIpQLSeL2pzmlY7m3L9E_SKCiwymwibTPeZicUZzelCqkXhXOMcB2A/viewform",
 	"hardware-poll": "https://hacksoc.typeform.com/to/a9Flsq",
