f96da218d07c353957954280c5dee30d6f871ba0
Aaron Osher <aaron@aaronosher.io>
2018-10-23 15:33:18 +0100

    Add python-4

--- redir_links.js
+++ redir_links.js
@@ -5,6 +5,7 @@ window.nexdivert = {
 	"hardware-poll": "https://hacksoc.typeform.com/to/a9Flsq",
 	"python-2": "https://github.com/monotron/python-workshops/blob/master/worksheet-session2.md",
 	"python-3": "https://github.com/monotron/python-workshops/blob/master/worksheet-session3.md",
+	"python-4": "https://github.com/monotron/python-workshops/blob/master/worksheet-session4.md",
 	"hacktoberfest-slides": "https://slides.com/aaronosher/hacksoc-hacktoberfest#/",
 	"discord": "https://discord.gg/kHg8qXB",
 	"intro-linux": "https://github.com/hacksocnotts/intro-to-linux/blob/master/Worksheet.md",
