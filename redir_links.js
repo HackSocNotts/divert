a4d4990a2230ad649453eb91607b9cbf541a7274
Aaron Osher <aaron@oshers.com>
2018-10-29 18:33:17 +0000

    Update redir_links.js

--- redir_links.js
+++ redir_links.js
@@ -1,7 +1,8 @@
 window.nexdivert = {
+	"edit": "https://github.com/HackSocNotts/divert/edit/gh-pages/redir_links.js",
 	"su": "https://www.su.nottingham.ac.uk/societies/society/hack/",
 	"october-1-cal": "https://drive.google.com/file/d/1njsHTOUkw6WhykOaZ8m8aO8CUqRlgjrC/view",
-	"cs-access": "https://docs.google.com/forms/d/e/1FAIpQLSeL2pzmlY7m3L9E_SKCiwymwibTPeZicUZzelCqkXhXOMcB2A/viewform",
+	"cs-access": "https://www.su.nottingham.ac.uk/societies/society/hack/cs-access/",
 	"hardware-poll": "https://hacksoc.typeform.com/to/a9Flsq",
 	"python-2": "https://github.com/monotron/python-workshops/blob/master/worksheet-session2.md",
 	"python-3": "https://github.com/monotron/python-workshops/blob/master/worksheet-session3.md",
